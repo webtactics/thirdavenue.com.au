5199a616d27a5f207e74384492b7404d8b1d0a68
webtactics <paul@webtactics.com.au>
2023-08-09 11:46:31 +1000

    2

--- _includes/assets/js/script.js
+++ _includes/assets/js/script.js
@@ -29,17 +29,7 @@ const toggleButton = document.querySelector('.toggle-nav');
   });
   
 
-const header = document.querySelector('.topbar');
-const scrollWatcher = document.createElement('div');
 
-scrollWatcher.setAttribute('data-scroll-watcher', '');
-header.before(scrollWatcher);
-
-const navObserver = new IntersectionObserver((entries) => {
-header.classList.toggle('scrolled', !entries[0].isIntersecting)
-
-  }, {rootMargin: "250px 0px 0px 0px"});
-navObserver.observe(scrollWatcher)
 
 
 
