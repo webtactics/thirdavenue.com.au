6d9fbcf66569e4ded737bea99862e954e120f728
webtactics <paul@webtactics.com.au>
2023-08-13 08:50:27 +1000

    to=top

--- _includes/assets/js/script.js
+++ _includes/assets/js/script.js
@@ -30,7 +30,24 @@ const toggleButton = document.querySelector('.toggle-nav');
   
 
 
-
+  $(document).ready(function(){
+	
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+      if ($(this).scrollTop() > 100) {
+        $('.scrollToTop').fadeIn();
+      } else {
+        $('.scrollToTop').fadeOut();
+      }
+    });
+    
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+      $('html, body').animate({scrollTop : 0},800);
+      return false;
+    });
+    
+  });
 
 
 
